8da26bf99f4a4e64b6ae92f03ac617df384aff82
BRENO OLIVEIRA ASSIS REIS <25133656@cfp127aluno.local>
2025-03-12 11:14:45 -0300

    Classe Quadrado. não esta rodando

---
+++ src/br/com/engsenai/CalculosGeometricos/Main.java
@@ -0,0 +1,11 @@
+package br.com.engsenai.CalculosGeometricos;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Quadrado quadrado = new Quadrado();
+	quadrado.setLado(2);
+
+	}
+
+}
